cc71de7ed8b1b2a26d22bfd70888d1532b35c4c2
Gaurav Bharti <gaurav.bharti4991@gmail.com>
2018-01-15 21:01:34 +0530

    MapBox - Directions API (Raw response)

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var request = require("request");
 
 var server = require('http').Server(app);
 
-app.get('/', function(req, res) {
-  res.send('Hello World');
+var mapboxToken = process.env.mapboxToken;
+
+app.get('/:sLong/:sLat/:dLong/:dLat', function(req, res) {
+  var URL = 'https://api.mapbox.com/directions/v5/mapbox/driving/'+req.params.sLong+','+req.params.sLat+';'+req.params.dLong+','+req.params.dLat+'.json?access_token='+mapboxToken;
+  request(URL, function (error, response, body) {
+		if(!error) {
+			var data = JSON.parse(body);
+			res.send(data);
+		}
+		else
+			res.status(404).send("Not found");
+	});
 });
 
-// Uncomment to add a new route which returns hello world as a JSON
-// app.get('/json', function(req, res) {
-//   res.json({
-//     message: 'Hello world'
-//   });
-// });
+app.get('/', function(req, res) {
+  res.send('Try the URL : '+req.get('host')+'/{Source Longitude}/{Source Latitude}/{Destination Longitude}/{Destination Latitude}');
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
