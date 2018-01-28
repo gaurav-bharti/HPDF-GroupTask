ce75db7b67e7637ef5f4b1597df665d6f33ee167
kailashnath1998 <kailashnath1998@gmail.com>
2018-01-28 22:40:03 +0530

    added forward gecoding

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -7,6 +7,18 @@ var server = require('http').Server(app);
 
 var mapboxToken = process.env.mapbox_token;
 
+app.get('/geocoding/forward/:location', function(req, res){
+	var URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/t+'+req.params.location+.json?access_token='+mapboxToken';
+	request(URL, function (error, response, body) {
+		if(!error) {
+			var data = JSON.parse(body);
+			res.send(data);
+		}
+		else
+			res.status(404).send("Not found");
+	});
+});
+
 app.get('/:sLong/:sLat/:dLong/:dLat', function(req, res) {
   var URL = 'https://api.mapbox.com/directions/v5/mapbox/driving/'+req.params.sLong+','+req.params.sLat+';'+req.params.dLong+','+req.params.dLat+'.json?access_token='+mapboxToken;
   request(URL, function (error, response, body) {
