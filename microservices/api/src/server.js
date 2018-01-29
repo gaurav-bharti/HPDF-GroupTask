11a58a70255a338c28c43da6caf445a4df2f7edd
kailashnath1998 <kailashnath1998@gmail.com>
2018-01-29 17:20:11 +0530

    added backward geocoding

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -21,6 +21,18 @@ app.get('/geocoding/forward/:location', function(req, res){
 	});
 });
 
+app.get('/geocoding/backward/:long/:lang', function(req, res){
+	var URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/t+'+req.params.long+','+req.params.lang+'.json?access_token='+mapboxToken;
+	request(URL, function (error, response, body) {
+		if(!error) {
+			var data = JSON.parse(body);
+			res.send(data);
+		}
+		else
+			res.status(404).send("Not found");
+	});
+});
+
 app.get('/:sLong/:sLat/:dLong/:dLat', function(req, res) {
   var URL = 'https://api.mapbox.com/directions/v5/mapbox/driving/'+req.params.sLong+','+req.params.sLat+';'+req.params.dLong+','+req.params.dLat+'.json?access_token='+mapboxToken;
   request(URL, function (error, response, body) {
