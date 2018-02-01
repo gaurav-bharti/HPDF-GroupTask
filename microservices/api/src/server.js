cc6b5571e1b38af8d12c8e4b304d3b176ee35662
Gaurav Bharti <gaurav.bharti4991@gmail.com>
2018-02-01 19:40:37 +0530

    App now uses temporary tokens for client side

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -4,13 +4,17 @@ var router = express.Router();
 var request = require("request");
 var path = require('path');
 
-var server = require('http').Server(app);
+var mapboxSecretToken = process.env.mapbox_token;
+var mapboxUsername = 'johndoe-';
+var mapboxPublicToken = '';
+//In minutes (Must be between 0 and 60)
+var tokenExpirationTime = 55;
+//Time in minutes to request a new token before the previous one has expired
+var tokenDifference = 5;
 
-//Public access token
-var mapboxToken = 'pk.eyJ1Ijoiam9obmRvZS0iLCJhIjoiY2pidXd1bm92MXJjZzJ3bzBsNGY4NGNvdSJ9.0qO3UooEMGh-UEQzS9Oyaw';//process.env.mapbox_token;
 
 app.get('/geocoding/forward/:location', function(req, res){
-	var URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/t+'+req.params.location+'.json?access_token='+mapboxToken;
+	var URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/t+'+req.params.location+'.json?access_token='+mapboxSecretToken;
 	request(URL, function (error, response, body) {
 		if(!error) {
 			var data = JSON.parse(body);
@@ -23,7 +27,7 @@ app.get('/geocoding/forward/:location', function(req, res){
 
 
 app.get('/geocoding/backward/:long/:lang', function(req, res){
-	var URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/+'+req.params.long+','+req.params.lang+'.json?access_token='+mapboxToken;
+	var URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/+'+req.params.long+','+req.params.lang+'.json?access_token='+mapboxSecretToken;
 	request(URL, function (error, response, body) {
 		if(!error) {
 			var data = JSON.parse(body);
@@ -34,13 +38,8 @@ app.get('/geocoding/backward/:long/:lang', function(req, res){
 	});
 });
 
-app.get('/publictoken/get', function(req, res){
-	var JSONstr = {"token":mapboxToken}
-	res.status(200).send(JSONstr)
-});
-
 app.get('/:sLong/:sLat/:dLong/:dLat', function(req, res) {
-  var URL = 'https://api.mapbox.com/directions/v5/mapbox/driving/'+req.params.sLong+','+req.params.sLat+';'+req.params.dLong+','+req.params.dLat+'.json?access_token='+mapboxToken;
+  var URL = 'https://api.mapbox.com/directions/v5/mapbox/driving/'+req.params.sLong+','+req.params.sLat+';'+req.params.dLong+','+req.params.dLat+'.json?access_token='+mapboxSecretToken;
   request(URL, function (error, response, body) {
 		if(!error) {
 			var data = JSON.parse(body);
@@ -51,9 +50,80 @@ app.get('/:sLong/:sLat/:dLong/:dLat', function(req, res) {
 	});
 });
 
+requestNewToken();
+
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, 'index.html'));
+	res.send(getIndexTemplate());
 });
+
+function addZero(data) {
+	return (('0' + data).slice(-2));
+}
+
+function getExpirationTime() {
+	var d = new Date();
+	d.setMinutes(d.getMinutes() + tokenExpirationTime);
+	return d.toISOString();
+}
+
+function requestNewToken() {
+	var URL = 'https://api.mapbox.com/tokens/v2/' + mapboxUsername + '?access_token=' + mapboxSecretToken;
+	request.post(URL, { json: { expires: ''+getExpirationTime() , scopes: ["styles:read", "fonts:read", "styles:tiles", "datasets:read"]} },
+			function (error, response, body) {
+				if(!error) {
+					mapboxPublicToken = body['token'];
+					setTimeout(tokenExpired, 1000*60*(tokenExpirationTime-tokenDifference));
+				}
+				else {
+					//Retry after sometime
+					setTimeout(requestNewToken, 1000*60*5);
+				}
+			}
+		);
+}
+
+function tokenExpired() {
+	requestNewToken();
+}
+
+function getIndexTemplate() {
+	var template = `<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8' />
+    <title>Display directions</title>
+    <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
+    <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.0/mapbox-gl.js'></script>
+    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.0/mapbox-gl.css' rel='stylesheet' />
+    <style>
+        body { margin:0; padding:0; }
+        #map { position:absolute; top:0; bottom:0; width:100%; }
+    </style>
+</head>
+<body>
+
+<script src='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v3.1.1/mapbox-gl-directions.js'></script>
+<link rel='stylesheet' href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v3.1.1/mapbox-gl-directions.css' type='text/css' />
+<div id='map'></div>
+
+<script>
+mapboxgl.accessToken = '${mapboxPublicToken}';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [80.237343,13.040383],
+    zoom: 3.5
+});
+
+map.addControl(new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+}), 'top-left');
+</script>
+
+</body>
+</html>`;
+return template;
+}
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
