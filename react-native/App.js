8a87e401c17cde73d1c85a2aa9195081b1663442
sagar-chandani <spc1194@gmail.com>
2018-02-04 23:20:18 +0530

    added React Native Project

---
+++ react-native/App.js
@@ -0,0 +1,206 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, AppRegistry } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Mapshow from './components/Mapshow';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+
+const homePlace = { description: 'Home', geometry: { location: { lat: 23.8152937, lng: 72.4597668 } }};
+const workPlace = { description: 'Work', geometry: { location: { lat: 23.496818, lng: 72.2940881 } }};
+const Ahmedabad = { description: 'Ahmedabad', geometry: { location: { lat: 23.022505, lng: 72.57136209999999 } }};
+const Delhi = { description: 'Delhi', geometry: { location: { lat: 28.7040592, lng: 77.10249019999992 } }};
+
+class HomeScreen extends React.Component {
+
+  static navigationOptions = {
+    title: 'Let\'s begin your journey!'
+  };
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      sourceDetails: {
+        srcLat: 0,
+        srcLong: 0
+      },
+      destDetails: {
+        destLat: 0,
+        destLong: 0
+      },
+    }
+  }
+
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+       (position) => {
+         console.log("wokeeey");
+         console.log(position);
+         this.setState({
+           latitude: position.coords.latitude,
+           longitude: position.coords.longitude,
+           error: null,
+         });
+
+       },
+       (error) => this.setState({ error: error.message }),
+       { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },
+     );
+
+
+}
+
+
+  render() {
+    const { navigate } = this.props.navigation;
+    return  (
+        <View style={{ margin: 20, padding: 30,flex: 1, justifyContent: 'space-between'}}>
+       <GooglePlacesAutocomplete
+         placeholder="Source"
+         minLength={2} // minimum length of text to search
+         autoFocus={false}
+         returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
+         listViewDisplayed="auto" // true/false/undefined
+         fetchDetails={true}
+         renderDescription={row => row.description} // custom description render
+         onPress={(data, description = null) => {
+           // 'details' is provided when fetchDetails = true
+           this.setState({
+             sourceDetails: {
+               srcLat: description.geometry.location.lat,
+               srcLong: description.geometry.location.lng
+             }
+           })
+           console.log(data);
+           console.log(description);
+           console.log(this.state.sourceDetails);
+         }}
+         getDefaultValue={() => {
+           return ''; // text input default value
+         }}
+         query={{
+           // available options: https://developers.google.com/places/web-service/autocomplete
+           key: 'AIzaSyC2QhtACfVZ2cr9HVvxQuzxd3HT36NNK3Q',
+           language: 'en', // language of the results
+           types: '(cities)', // default: 'geocode'
+         }}
+         styles={{
+           description: {
+             fontWeight: 'bold',
+             alignItems: 'center'
+           },
+           predefinedPlacesDescription: {
+             color: '#1faadb',
+             justifyContent: 'center',
+             flex: 1,
+             alignItems: 'center'
+           },
+         }}
+         // currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+         // currentLocationLabel="Current location"
+         nearbyPlacesAPI="GooglePlacesSearch" // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+         GoogleReverseGeocodingQuery={{
+           // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+         }}
+         GooglePlacesSearchQuery={{
+           // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+           rankby: 'distance',
+           types: 'food',
+         }}
+         filterReverseGeocodingByTypes={[
+           'locality',
+           'administrative_area_level_3',
+         ]} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+         predefinedPlaces={[homePlace, workPlace,Ahmedabad, Delhi]}
+         debounce={200}
+       />
+
+       <GooglePlacesAutocomplete
+         placeholder="Destination"
+         minLength={2} // minimum length of text to search
+         autoFocus={false}
+         returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
+         listViewDisplayed="auto" // true/false/undefined
+         fetchDetails={true}
+         renderDescription={row => row.description} // custom description render
+         onPress={(data, description = null) => {
+           // 'details' is provided when fetchDetails = true
+           this.setState({
+             destDetails: {
+               destLat: description.geometry.location.lat,
+               destLong: description.geometry.location.lng
+             }
+           })
+           console.log(data);
+           console.log(description);
+           console.log(this.state.destDetails);
+         }}
+         getDefaultValue={() => {
+           return ''; // text input default value
+         }}
+         query={{
+           // available options: https://developers.google.com/places/web-service/autocomplete
+           key: 'AIzaSyC2QhtACfVZ2cr9HVvxQuzxd3HT36NNK3Q',
+           language: 'en', // language of the results
+           types: '(cities)', // default: 'geocode'
+         }}
+         styles={{
+           description: {
+             fontWeight: 'bold',
+           },
+           predefinedPlacesDescription: {
+             color: '#1faadb',
+             alignItems: 'center'
+           },
+         }}
+         // currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+         // currentLocationLabel="Current location"
+         nearbyPlacesAPI="GooglePlacesSearch" // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+         GoogleReverseGeocodingQuery={{
+           // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+         }}
+         GooglePlacesSearchQuery={{
+           // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+           rankby: 'distance',
+           types: 'food',
+         }}
+         filterReverseGeocodingByTypes={[
+           'locality',
+           'administrative_area_level_3',
+         ]} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+         predefinedPlaces={[homePlace, workPlace ,Ahmedabad, Delhi]}
+         debounce={200}
+       />
+          <Button
+            onPress={() => navigate('Mapshow', {srcLat:this.state.sourceDetails.srcLat,
+              srcLong:this.state.sourceDetails.srcLong,
+              destLat: this.state.destDetails.destLat,
+              destLong: this.state.destDetails.destLong}) }
+            title="GO!!"
+          />
+      </View>
+    );
+  }
+}
+
+const SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen },
+  Mapshow: { screen: Mapshow },
+});
+
+export default class App extends React.Component {
+  render() {
+    return <SimpleApp />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
+
+
+AppRegistry.registerComponent('blog', () => App);
---
+++ react-native/components/Mapshow.js
@@ -0,0 +1,211 @@
+import React from 'react';
+import {  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions, } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import MapViewDirections from 'react-native-maps-directions'
+import MapView  from 'react-native-maps';
+import Button from './Button';
+import axios from 'axios';
+import Card from './Card';
+import CardSection from './CardSection'
+import Panel from './Panel';
+const { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 23.0082;
+const LONGITUDE = 72.9784;
+const LATITUDE_DELTA = 20;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const GOOGLE_MAPS_APIKEY = 'AIzaSyCcd_KFCyDz_570_OSqwjmLm8Z6fE1rQf4';
+
+class Mapshow extends React.Component {
+
+  state = { routes: [] };
+
+  static navigationOptions = ({ navigation }) => ({
+      title: `Enjoy your Journey`,
+
+    });
+
+  constructor(props) {
+   super(props)
+   this.state = {
+     srcLoc: {
+         latitude: this.props.navigation.state.params.srcLat,
+         longitude: this.props.navigation.state.params.srcLong,
+       },
+       destLoc: {
+         latitude: 23.565,
+         longitude: 72.984,
+       }
+   };
+   console.log(this.props.navigation.state.params);
+ }
+
+
+
+componentWillMount() {
+  this.setState({
+    srcLoc: {
+        latitude: this.props.navigation.state.params.srcLat,
+        longitude: this.props.navigation.state.params.srcLong,
+      },
+      destLoc: {
+        latitude: this.props.navigation.state.params.destLat,
+        longitude: this.props.navigation.state.params.destLong,
+      },
+      routes : []
+  });
+  this.renderRoutes = function renderRoutes() {
+        return this.state.routes.map(route =>
+          <Route key={'toRoute'} route={route} />);
+    }
+  axios.get(`https://api.airdrop46.hasura-app.io/${this.props.navigation.state.params.srcLong}/${this.props.navigation.state.params.srcLat}/${this.props.navigation.state.params.destLong}/${this.props.navigation.state.params.destLat}`)
+       .then(response => this.setState({ routes: response.data.routes }));
+  console.log(this.state);
+}
+
+
+
+
+  render() {
+ console.log(this.state,'test');
+    const {state} = this.props.navigation;
+    const cord = {
+      latitude: state.params.srcLat,
+      longitude:state.params.srcLong,
+    }
+
+    return (
+      <View style={styles.viewStyle}>
+        <View style={styles.map}>
+      <MapView
+          initialRegion={{
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }}
+          zoomLevel={200}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+          style={StyleSheet.absoluteFill}>
+          <MapView.Marker coordinate={this.state.srcLoc} />
+           <MapView.Marker coordinate={this.state.destLoc} />
+    <MapViewDirections
+      origin={this.state.srcLoc}
+      destination={this.state.destLoc}
+      strokeColor="hotpink"
+      strokeWidth={4}
+      apikey={GOOGLE_MAPS_APIKEY}/>
+        </MapView>
+        </View >
+        <Text>Directions</Text>
+          <ScrollView >
+            {this.renderRoutes()}
+            </ScrollView>
+        </View>
+
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  map: {
+   width: Dimensions.get('window').width,
+   height: Dimensions.get('window').height/2
+ },
+ layout: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.05)',
+  },
+ viewStyle: {
+   justifyContent: 'space-between',
+   flexDirection: 'column',
+   flex: 1,
+   alignItems: 'center',
+   margin: 2,
+   padding: 2
+ }
+});
+
+
+class Route extends React.Component {
+
+constructor(props){
+  super(props)
+    this.state= this.props;
+
+}
+componentWillMount(){
+  this.renderLegs = function(){
+    return this.state.route.legs.map(leg =>
+      <Legs key={'toLeg'} leg={leg} />);
+  }
+}
+render() {
+  return (
+    this.renderLegs()
+  );
+}
+}
+
+
+class Legs extends React.Component {
+
+constructor(props){
+  super(props)
+    this.state= this.props;
+
+}
+componentWillMount(){
+  this.renderSteps = function(){
+    return this.state.leg.steps.map(step =>
+      <Step key={'toStep'} step={step} />);
+  }
+}
+render() {
+  return (
+    this.renderSteps()
+  );
+}
+}
+
+class Step extends React.Component {
+
+constructor(props){
+  super(props)
+    this.state= this.props;
+
+}
+// componentWillMount(){
+//   this.renderManeuver = function(){
+//     return this.state.step.maneuver.map(maneuver =>
+//       <Manuever maneuver={maneuver} />);
+//   }
+// }
+render() {
+  return (
+
+    <Card >
+      <CardSection>
+          <Text>{this.state.step.maneuver.instruction}</Text>
+          </CardSection>
+          <CardSection style={{backgroundColor: 'whitesmoke'}}>
+            <Text> and continue for {this.state.step.duration} metres</Text>
+            </CardSection>
+          </Card>
+);
+}
+}
+
+
+export default Mapshow;
